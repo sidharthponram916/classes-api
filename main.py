20c1839d525efae6a6f34bcf6eea508e1e3b05f6
Sidharth Ponram <sidpo@sidhartslaptop2.umd.edu>
2025-02-21 16:31:06 -0500

    update default date

--- main.py
+++ main.py
@@ -23,11 +23,11 @@ def read_root():
     return { "Message": "Welcome to the Testudo Schedule of Classes API"}
 
 @app.get("/api/get-courses")
-def get_course_information(name: str, date: str = "202501"):
+def get_course_information(name: str, date: str = "202508"):
     url = f'https://app.testudo.umd.edu/soc/search?courseId={name}&sectionId=&termId={date}&_openSectionsOnly=on&creditCompare=%3E%3D&credits=0.0&courseLevelFilter=ALL&instructor=&_facetoface=on&_blended=on&_online=on&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&teachingCenter=ALL&_classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on'
     return scraper.get_course_info(url)
 
 @app.get("/api/get-gen-ed-courses")
-def get_gen_ed_courses(name: str, date: str = "202501"): 
+def get_gen_ed_courses(name: str, date: str = "202508"): 
     url = f'https://app.testudo.umd.edu/soc/gen-ed/{date}/{name}'
     return scraper.get_gen_ed_courses(url)
